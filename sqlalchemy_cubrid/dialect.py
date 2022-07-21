1be2071afefd0c2062db7cb1a9e12b7b05736da9
Yeongsoen Choe <yeongseon.choe@gmail.com>
2022-07-21 07:48:30 +0000

    Add dbapi, create_connection_args

--- sqlalchemy_cubrid/dialect.py
+++ sqlalchemy_cubrid/dialect.py
@@ -19,7 +19,7 @@ from sqlalchemy_cubrid.base import (
 
 class CubridDialect(default.DefaultDialect):
     name = "cubrid"
-    driver = "cubrid"
+    driver = "CUBRID-Python"
 
     statement_compiler = CubridCompiler
     ddl_compiler = CubridDDLCompiler
@@ -31,5 +31,42 @@ class CubridDialect(default.DefaultDialect):
     def __init__(self, **kwargs):
         super(CubridDialect, self).__init__(**kwargs)
 
+
+    @classmethod
+    def dbapi(cls):
+        """Hook to the dbapi2.0 implementation's module"""
+        try:
+            import CUBRIDdb as cubrid_dbapi
+        except ImportError as e:
+            raise e
+        return cubrid_dbapi
+
+    def create_connect_args(self, url):
+        """
+        Build DB-API compatible connection arguments.
+        Overrides interface
+        :meth:`~sqlalchemy.engine.interfaces.Dialect.create_connect_args`.
+        """
+
+        # Connection to CUBRID database is made through connect() method.
+        # Syntax:
+        # connect (url, user, password])
+        #    url - CUBRID:host:port:db_name:db_user:db_password:::
+        #    user - Authorized username.
+        #    password - Password associated with the username.
+
+        if url is not None:
+            params = super(CubridDialect, self).create_connect_args(url)[1]
+            print(params)
+
+            args = (
+                'CUBRID:localhost:33000:testdb:::',
+                'dba',
+                '1234'
+            )
+            kwargs = {
+            }
+            return args, kwargs
+          
 
 dialect = CubridDialect
