6686bc497e62221a78c885991b15899de0901825
yeongseon <yeongseon.choe@gmail.com>
2022-08-22 23:46:51 +0900

    Update install_cubrid_python.sh

--- install_cubrid_python.sh
+++ install_cubrid_python.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-curl https://ftp.cubrid.org/CUBRID_Drivers/Python_Driver/11.1.0/Linux/cubrid-python-11.1-latest.tar.gz --output cubrid-python.tar.gz
-tar xvfz cubrid-python.tar.gz
-cd RB-11.1.0
-python setup.py build
-python setup.py install
+1. wget https://ftp.cubrid.org/CUBRID_Drivers/Python_Driver/9.3.0/Linux/cubrid-python-9.3.0.0001.tar.gz
+2. tar xvfz cubrid-python-9.3.0.0001.tar.gz
+3. cd RB-9.3.0
+4. CUBRIDdb/cursor.py
+    def __del__(self):
+        try:
+            if self._cs:
+                self.close()
+        except AttributeError:   # self._cs not exists
+            pass
+5. python setup.py build
+6. python setup.py install
